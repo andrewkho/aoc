3597583b8dbc427f8069bf209a0329d4a847991d
andrewkho <andrew.kenneth.ho@gmail.com>
2022-10-18 09:56:10 -0400

    pretty

--- 2015/go/day04.go
+++ 2015/go/day04.go
@@ -3,13 +3,27 @@ package main
 import (
 	"bufio"
 	"crypto/md5"
-	"encoding/hex"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"time"
 )
+
+func findKey(prefix string, mask []int) int {
+outer:
+	for i := 0; true; i++ {
+		hash := md5.Sum([]byte(fmt.Sprintf("%s%d", prefix, i)))
+		for i, m := range mask {
+			b := hash[i]
+			if int(b)&m != 0 {
+				continue outer
+			}
+		}
+		return i
+	}
+	return 0
+}
 
 func main() {
 	var filename string
@@ -40,45 +54,12 @@ func main() {
 	fmt.Printf(" took %v\n", time.Since(t0))
 
 	t1 := time.Now()
-	var i int
-
-	mb, err := hex.DecodeString("f0")
-	if err != nil {
-		log.Fatal(err)
-	}
-	mask := int(mb[0])
-
-outer:
-	for i = 0; true; i++ {
-		if i%200000 == 0 {
-			fmt.Println("iter:", i)
-		}
-		hash := md5.Sum([]byte(fmt.Sprintf("%s%d", line, i)))
-		for _, b := range hash[:2] {
-			if int(b) != 0 {
-				continue outer
-			}
-		}
-		if int(hash[2])&mask != 0 {
-			continue outer
-		}
-		break
-	}
+	mask := []int{255, 255, 255 - 15} // 0xf0
+	i := findKey(line, mask)
 	fmt.Printf("1: %v, %v\n", i, time.Since(t1))
 
 	t2 := time.Now()
-outer2:
-	for i = 0; true; i++ {
-		if i%2000000 == 0 {
-			fmt.Println("iter:", i)
-		}
-		hash := md5.Sum([]byte(fmt.Sprintf("%s%d", line, i)))
-		for _, b := range hash[:3] {
-			if int(b) != 0 {
-				continue outer2
-			}
-		}
-		break
-	}
+	mask = []int{255, 255, 255}
+	i = findKey(line, mask)
 	fmt.Printf("2: %v, %v\n", i, time.Since(t2))
 }
