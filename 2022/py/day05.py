5a54a2309c9dc498bf2a4b35cc3e6527b7ff85f2
andrewkho <andrew.kenneth.ho@gmail.com>
2022-12-04 21:40:03 -0800

    2022 day05.py

---
+++ 2022/py/day05.py
@@ -0,0 +1,52 @@
+from timer import Timer
+import re
+
+
+def main():
+    infile = "../inputs/" + __file__.replace(".py", ".txt")
+
+    with Timer(f"Reading from {infile}..."):
+        stacks = [list() for _ in range(10)]
+        instrs = []
+        _section1 = True
+        matcher = re.compile(r"^move ([0-9]+) from ([0-9]+) to ([0-9]+)$")
+        with open(infile, "r") as f:
+            lines = f.readlines()
+
+        for line in lines:
+            if _section1:
+                if line.strip() == "":
+                    _section1 = False
+                    continue
+                for i, stack in enumerate(stacks[1:]):
+                    pos = 1 + i*4
+                    if len(line) <= pos:
+                        break
+                    if line[pos] != " ":
+                        stack.append(line[pos])
+            else:
+                m = matcher.match(line.strip())
+                groups = m.groups()
+                instrs.append((int(groups[0]), int(groups[1]), int(groups[2])))
+        stacks = [stack[::-1][1:] for stack in stacks]
+
+    with Timer("part 1"):
+        stacks_orig = [stack.copy() for stack in stacks]
+
+        for n, src, dst in instrs:
+            for _ in range(n):
+                stacks[dst].append(stacks[src].pop())
+
+        print("part 1:", ''.join(stack[-1] for stack in stacks[1:]) )
+
+    with Timer("part 2"):
+        stacks = stacks_orig
+        for n, src, dst in instrs:
+            stacks[dst].extend(stacks[src][-n:])
+            stacks[src] = stacks[src][:len(stacks[src])-n]
+
+        print("part 2:", ''.join(stack[-1] for stack in stacks[1:]) )
+
+
+if __name__ == '__main__':
+    main()
