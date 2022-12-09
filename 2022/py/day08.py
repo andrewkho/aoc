ee95575c2709d54c68ee7c2627806097bc15dd21
andrewkho <andrew.kenneth.ho@gmail.com>
2022-12-08 22:35:42 -0800

    day08 with single pass monostack

--- 2022/py/day08.py
+++ 2022/py/day08.py
@@ -23,73 +23,79 @@ def main():
             return False
             
         for i in range(W):
-            for rr in [range(H), range(H-1, -1, -1)]:
-                stack = []
-                for j in rr:
-                    if step(stack, i, j):
-                        break
+            j = 0
+            stack = []
+            while j < H and not step(stack, i, j):
+                j+=1
+            j = H-1
+            stack = []
+            while j >= 0 and not step(stack, i, j):
+                j-=1
 
         for j in range(H):
-            for rr in [range(W), range(W-1, -1, -1)]:
-                stack = []
-                for i in rr:
-                    if step(stack, i, j):
-                        break
+            i = 0
+            stack = []
+            while i < W and not step(stack, i, j):
+                i += 1
+            i = H-1
+            stack = []
+            while i >= 0 and not step(stack, i, j):
+                i -= 1
 
         print("part 1:", len(seen))
 
     with Timer("part 2"):
         totals = [[1]*W for _ in range(H)]
 
+        # Single pass with monostack
         for j in range(H):
-            # left to right
             stack = []
             for i in range(W):
-                while len(stack) and lines[j][i] > stack[-1][0]:
-                    stack.pop()
-                if len(stack) == 0:
-                    v = i
-                else:
-                    v = i-stack[-1][1]
-                totals[j][i] *= v
-                stack.append((lines[j][i], i))
+                h = lines[j][i]
+                prev = h
+                while stack and h > stack[-1][0]:
+                    if stack[-1][0] > prev:
+                        totals[j][stack[-1][1]] *= i-stack[-1][1]
+                    prev = stack.pop()[0]
+                
+                totals[j][i] *= i-stack[-1][1] if stack else i
 
-            # right to left
-            stack = []
-            for i in range(W-1, -1, -1):
-                while len(stack) and lines[j][i] > stack[-1][0]:
-                    stack.pop()
-                if len(stack) == 0:
-                    v = W - 1 - i
-                else:
-                    v = stack[-1][1] - i 
-                totals[j][i] *= v
-                stack.append((lines[j][i], i))
+                while stack and h == stack[-1][0]:
+                    if stack[-1][0] > prev:
+                        totals[j][stack[-1][1]] *= i-stack[-1][1]
+                    prev = stack.pop()[0]
+
+                stack.append((h, i))
+            
+            prev = stack.pop()[0]
+            while stack:
+                if stack[-1][0] > prev:
+                    totals[j][stack[-1][1]] *= W-1 - stack[-1][1]
+                prev = stack.pop()[0]
         
         for i in range(W):
-            # up to down
             stack = []
             for j in range(H):
-                while len(stack) and lines[j][i] > stack[-1][0]:
-                    stack.pop()
-                if len(stack) == 0:
-                    v = j
-                else:
-                    v = j-stack[-1][1]
-                totals[j][i] *= v
-                stack.append((lines[j][i], j))
+                h = lines[j][i]
+                prev = h
+                while len(stack) and h > stack[-1][0]:
+                    if stack[-1][0] > prev:
+                        totals[stack[-1][1]][i] *= j-stack[-1][1]
+                    prev = stack.pop()[0]
+                    
+                totals[j][i] *= j-stack[-1][1] if stack else j
 
-            # down to up
-            stack = []
-            for j in range(H-1, -1, -1):
-                while len(stack) and lines[j][i] > stack[-1][0]:
-                    stack.pop()
-                if len(stack) == 0:
-                    v = H-1-j
-                else:
-                    v = stack[-1][1] - j
-                totals[j][i] *= v
-                stack.append((lines[j][i], j))
+                while len(stack) and h == stack[-1][0]:
+                    if stack[-1][0] > prev:
+                        totals[stack[-1][1]][i] *= j-stack[-1][1]
+                    prev = stack.pop()[0]
+                stack.append((h, j))
+            
+            prev = stack.pop()[0]
+            while stack:
+                if stack[-1][0] > prev:
+                    totals[stack[-1][1]][i] *= H-1 - stack[-1][1]
+                prev = stack.pop()[0]
 
         best = max(max(x for x in line) for line in totals)
         print("part 2:", best)
