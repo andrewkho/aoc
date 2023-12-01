62390ef9efd1a91f97eba549c94ba9a4f573ed76
andrewkho <andrew.kenneth.ho@gmail.com>
2023-11-30 21:32:04 -0800

    day01.py

--- 2023/py/day01.py
+++ 2023/py/day01.py
@@ -1,6 +1,8 @@
 from timer import Timer
 from utils import get_input
 
+import re
+
 
 def main():
     infile = get_input(__file__)
@@ -9,14 +11,54 @@ def main():
             lines = []
             for line in f.readlines():
                 lines.append(line.strip())
-
+    
     with Timer("part 1"):
-        pass
-        # print("part1:", result)
+        tot = 0
+        for line in lines:
+            digits = []
+            for c in line:
+                if c.isnumeric():
+                    digits.append(c)
+                    break
+            for c in reversed(line):
+                if c.isnumeric():
+                    digits.append(c)
+                    break
+            assert len(digits) == 2
+            tot += int(''.join(digits))
+            
+        print("part1:", tot)
+
 
     with Timer("part 2"):
-        pass
-        # print("part 2:", result)
+        tot = 0
+        pattern = r"(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))"
+        vals = {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+        }
+        for line in lines:
+            m = re.findall(pattern, line)
+            digits = []
+            if m[0].isnumeric():
+                digits.append(m[0])
+            else:
+                digits.append(str(vals[m[0]]))
+
+            if m[-1].isnumeric():
+                digits.append(m[-1])
+            else:
+                digits.append(str(vals[m[-1]]))
+            assert len(digits) == 2
+            tot += int(''.join(digits))
+        print("part 2:", tot)
 
 
 if __name__ == '__main__':
